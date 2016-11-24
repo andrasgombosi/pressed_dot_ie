ba6b1183c1801315d156b8a359f3014276ea64e7
Andras Gombosi <andras.gombosi@gmail.com>
2016-11-24 16:15:36 +0000

    WordCloud is working!

--- mood_phase_1.py
+++ mood_phase_1.py
@@ -7,6 +7,8 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 from bs4 import UnicodeDammit
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 def cleanse_href(href_str, base_url):
     """
@@ -21,7 +23,6 @@ def cleanse_href(href_str, base_url):
         href_str = href_str.strip()
     except AttributeError:
         ret_val = False
-
 
 # getting rid of single digit , typically # hrefs
     if ret_val and len(href_str) > 1:
@@ -53,7 +54,7 @@ os.system('cls')
 TOP_DOMAIN = "http://www.irishtimes.com"
 
 HTML_DOC = urllib.request.urlopen(TOP_DOMAIN)
-SOUP = BeautifulSoup(HTML_DOC, 'html.parser')
+SOUP = BeautifulSoup(HTML_DOC, 'lxml')
 
 #get the URLs out, but only the ones which are pointing to a page under hte same domain
 #def only_this_domain(href):
@@ -72,15 +73,40 @@ for link in SOUP.find_all('a'):
     if href_content_clean is not False:
         #only append it once
         if href_content_clean not in URL_LIST:
-            URL_LIST.append( href_content_clean)
+            URL_LIST.append(href_content_clean)
 
 # so, at this stage I have a full list of URL's which point to actual articles
-for i in range(0, len(URL_LIST)):
-    print("%d element: %s " % (i, URL_LIST[i]))
+
+# for i in range(0, len(URL_LIST)):
+#    print("%d element: %s " % (i, URL_LIST[i]))
 
 # right, get the first URL, and extract the words
 
 CHILD_DOC = urllib.request.urlopen(URL_LIST[0])
-CHILD_SOUP = BeautifulSoup(CHILD_DOC, 'html.parser')
+CHILD_SOUP = BeautifulSoup(CHILD_DOC, 'lxml')
 
-print (CHILD_SOUP.getText())
+#this is irishtimes specific again..
+ARTICLE = CHILD_SOUP.find('article')
+#print (ARTICLE.getText())
+
+# so, the wordcloud package takes a text file as input, let's create one
+#TEXT_FILE = open('text.txt','w')
+#TEXT_FILE.write(ARTICLE.getText())
+# Read the whole text.
+
+TEXT = ARTICLE.getText()
+# Generate a word cloud image
+WORDCLOUD = WordCloud().generate(TEXT)
+
+# Display the generated image:
+# the matplotlib way:
+
+plt.imshow(WORDCLOUD)
+plt.axis("off")
+
+# lower max_font_size
+WORDCLOUD = WordCloud(max_font_size=40).generate(TEXT)
+plt.figure()
+plt.imshow(WORDCLOUD)
+plt.axis("off")
+plt.show()
